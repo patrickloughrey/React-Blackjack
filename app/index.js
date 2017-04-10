2a12b23ab60314ebc9ae171564b46f8a3888c661
Patrick Loughrey <PatrickLoughrey@Patricks-MacBook-Pro-2.local>
2017-04-10 18:42:33 -0400

    Created webpack script in package.json

--- app/index.js
+++ app/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 ReactDOM.render(
-    <App />
-    document.getElementById('app');
+    <App />,
+    document.getElementById('app')
 );
