9dceca9af55ffe2c4bc1f568dc8258aa6a1b1d97
Patrick Loughrey <PatrickLoughrey@Patricks-MBP-2.home>
2017-04-10 21:58:28 -0400

    Created testing environment with Mocha

---
+++ test/lib/cards_spec.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { List } from 'immutable';
+
+import { newDeck } from '../../lib/cards';
+
+/* Write test using Mocha & Chai:
+   Go to 'https://mochajs.org/' to see syntax */
+
+describe('cards.js', () => {
+    describe('newDeck', () => {
+        it('the deck is an immutable list', () => {
+            expect(newDeck()).to.be.instanceOf(List);
+        });
+        it('the deck starts out with 52 cards', () => {
+            expect(newDeck().size).to.eq(52);
+        });
+    });
+});
