645820d43395bf1534d2804c6f0037d5797fe960
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-13 15:40:20 -0400

    Hand component working

---
+++ app/components/hand.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Hand extends React.Component {
+    render() {
+        return(
+            <p> This is the Hand component </p>
+        );
+    }
+};
