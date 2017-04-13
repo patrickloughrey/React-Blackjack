0c6569b878639d8dddf9c3b165029b7833fd7ce5
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-13 15:37:49 -0400

    Created tests for Info and Hand components

--- test/components/app_spec.js
+++ test/components/app_spec.js
@@ -9,10 +9,37 @@ import { newDeck, deal } from '../../app/lib/cards';
 let deck = newDeck();
 let playerHand, dealerHand;
 
+[deck, playerHand] = deal(deck, 2);
+[deck, dealerHand] = deal(deck, 2);
+
+const state = fromJS({
+    deck,
+    playerHand,
+    dealerHand,
+    "winCount": 0,
+    "lossCount": 0,
+    hasStood: false
+});
+
 describe('<App />', () => {
     const rendered = shallow(<App />);
 
     it('renders <Info /> component', () => {
         expect(rendered.find('Info')).to.have.length(1);
     });
+
+    it('passes props to <Info />', () => {
+        const info = rendered.find('Info').first();
+        expect(info).to.have.prop('winCount',
+            state.get('winCount'));
+        expect(info).to.have.prop('lossCount', 
+            state.get('lossCount'));
+        expect(info).to.have.prop('hasStood',
+            state.get('hasStood'));
+    });
+
+    it('renders two <Hand /> components', () => {
+        expect(rendered.find('Hand')).to.have.length(2);
+    });
+    
 });
