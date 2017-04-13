9c30e9a8a89806f5be8192afd76cc8018d5e465d
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-12 22:41:21 -0400

    Passing state variables to Info component

--- test/components/app_spec.js
+++ test/components/app_spec.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import { fromJS } from 'immutable';
 
 import App from '../../app/components/app';
+import { newDeck, deal } from '../../app/lib/cards';
+
+let deck = newDeck();
+let playerHand, dealerHand;
 
 describe('<App />', () => {
     const rendered = shallow(<App />);
