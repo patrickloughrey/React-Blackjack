5fd62d4a9b1b5a6a1210e9af5ac2bc46bf069f56
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-13 16:13:12 -0400

    Set up hit and stay buttons in the Info component

--- app/components/info.js
+++ app/components/info.js
@@ -4,10 +4,21 @@ export default class Info extends React.Component {
     render() {
         return(
             <div id="info">
+
               <span id="player_record">
                 Wins: {this.props('winCount')}
                 Losses: {this.props('lossCount')}
               </span>
+
+              <span id="buttons">
+                  <button disabled={this.props.hasStood}> 
+                      Hit 
+                  </button>
+                  <button disabled={this.props.hasStood}> 
+                      Stay 
+                  </button>
+              </span>
+
             </div>
         );
     }
--- test/components/info_spec.js
+++ test/components/info_spec.js
@@ -5,10 +5,39 @@ import { shallow } from 'enzyme';
 import Info from '../../app/components/info';
 
 describe('<Info />', () => {
-    const rendered = shallow(<Info winCount={1} lossCount={2} hasStood={false} />);
+  describe('when hasStood is false, when player wishes to hit' , () => { 
+      const rendered = shallow(<Info winCount={1} lossCount={2} hasStood={false} />);
 
-    it('displays wins/losses record', () => {
+      it('displays wins/losses record', () => {
         expect(rendered).to.include.text("Wins: 1");
         expect(rendered).to.include.text("Losses: 2");
-    });
+      });
+
+      const buttons = rendered.find('button');
+      it('displays hit stay buttons', () => {
+        const buttons = rendered.find('button');
+        expect(buttons).to.have.length(2);
+        expect(buttons.first()).to.have.text('Hit');
+        expect(buttons.last()).to.have.text('Stay');
+      });
+
+      it('enables the hit and stay buttons', () => {
+        buttons.forEach((x) => {
+            expect(x).to.not.have.attr('disabled');
+        });
+      });
+
+  });
+
+  describe('when hasStood is true, when player wishes to stay', () => {
+      const rendered = shallow(<Info winCount={1} lossCount={2} hasStood={false} />);
+
+      it('disables the hit and stay buttons', () => {
+        const buttons = rendered.find('button');
+        buttons.forEach((x) => {
+            expect(x).to.have.attr('disabled');
+        });
+      });
+  })
+
 });
