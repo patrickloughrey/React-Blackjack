50ed4120120dac77e8050809f317bc2d75b3337e
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-17 17:41:27 -0400

    Created dummy card test for dealer's first card

--- test/components/hand_spec.js
+++ test/components/hand_spec.js
@@ -12,17 +12,34 @@ const n = 2;
 [deck, hand] = deal(deck, n);
 
 describe('<Hand />', () => {
-    const rendered = shallow(<Hand cards={hand} />);
-    const cards = rendered.find('Card');
+    describe('without dummy cards', () => {
+        const rendered = shallow(<Hand cards={hand} />);
+        const cards = rendered.find('Card');
 
-    it('renders correct number of cards', () => {
-        expect(cards).to.have.length(n);
-    });
+        it('renders correct number of cards', () => {
+            expect(cards).to.have.length(n+1);
+        });
 
-    it('gives each card the correct number of props', () => {
-        hand.forEach((card, i) => {
-            expect(cards.at(i)).to.have.prop('suit', card.get('suit'));
-            expect(cards.at(i)).to.have.prop('rank', card.get('rank'));
+        it('gives each card the correct number of props', () => {
+            hand.forEach((card, i) => {
+                expect(cards.at(i)).to.have.prop('suit', card.get('suit'));
+                expect(cards.at(i)).to.have.prop('rank', card.get('rank'));
+                expect(cards.at(i)).to.have.prop('faceDown', false);
+            });
         });
     });
+
+    describe('with dummy cards', () => {
+        const rendered = shallow(<Hand cards={hand.push(new Map())} />);
+        const cards = rendered.find('Card');
+
+        it('renders correct number of cards', () => {
+            expect(cards).to.have.length(n+1);
+        });
+
+        it('gives dummy card faceDown=true', () => {
+            expect(cards.last()).to.have.prop('faceDown', true);
+        });
+    });
+
 });
