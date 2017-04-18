7f4ad5cb9e97abb155b31b9e10477dda244bfb8f
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-17 22:28:19 -0400

    Set up a test for reducer

---
+++ test/reducer_spec.js
@@ -0,0 +1,35 @@
+import { Map } from 'immutable';
+import { expect } from 'chai';
+
+import reducer from '../app/reducer';
+
+describe('reducer', () => {
+    describe('SetUpGame', () => {
+        const action = {
+            type: 'SetUpGame'
+        };
+
+        describe('initially empty state', () => {
+            const initialState = undefined;
+            const nextState = reducer(initialState, action); /* Mandatory parameters passed to redux's reducer method */
+
+            it('sets up deck of cards', () => {
+                expect(nextState.get('deck').size).to.eq(49);
+            });
+
+            it('set up playerHand', () => {
+                expect(nextState.get('playerHand').size).to.eq.(2);
+            });
+
+            it('set up dealerHand', () => {
+                expect(nextState.get('dealerHand').size).to.eq(2);
+                expect(nextState.get('dealerHand').last()).to.eq(new Map());
+            });
+
+            it('sets up hasStood', () => {
+                expect(nextState.get('hasStood')).to.eq(false);
+            })
+
+        });
+    });
+});
