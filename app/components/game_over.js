e9ef9f31fd60e14b6b72df3501890dfa7d37176c
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-26 23:42:13 -0400

    Set up next game button

--- app/components/game_over.js
+++ app/components/game_over.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 /* JSX will not let you do multiple if-else statements within the return function.
    So, we do it outside of return, save it to a variable, then pass the variable as a prop */
-export class GameOverMessage extends React.Component {
+export default class GameOverMessage extends React.Component {
     render() {
         let message;
 
@@ -19,6 +19,7 @@ export class GameOverMessage extends React.Component {
         return(
             <div id="game_over_message">
                 { message }
+                <button onClick={this.props.nextGame}>Next Game</button>
             </div>
         );
     }
