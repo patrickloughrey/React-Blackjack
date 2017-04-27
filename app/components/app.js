94fe64d607ae597b1b6da4ef3f8c828cbda03ce9
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-04-27 00:49:28 -0400

    Added game over message to app component

--- app/components/app.js
+++ app/components/app.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { InfoContainer } from './info';
 import Hand from './hand';
 import { connect } from 'react-redux';
+import { GameOverMessage } from './game_over';
 
 export class App extends React.Component {
     render() {
         console.log(this.props);
+        let messageComponent;
+
+        if(this.props.gameOver) {
+            messageComponent = <GameOverMessage win={this.props.playerWon} />;
+        }
+
         return (
           <div className="app">
               <h1> React Blackjack </h1>
               <InfoContainer />
+                { messageComponent }
                 <strong> Player's Hand: </strong>
                 <Hand cards={this.props.playerHand} />
                 <strong> Dealer's Hand: </strong>
