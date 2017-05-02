736ec4c1fd8be533edd88e0b5d1ad134abdd0196
Tony Carnese <ajcarnese@gmail.com>
2017-05-02 19:42:09 -0400

    server.js is functional. needs styling route fixed

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,57 @@
 *
 *
 */
+/* Including Passport */
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+/* Require index.js */
+var db = require("../models/Game");
+/* Local Strategy is having the user login with email and password */
+passport.use(new LocalStrategy( 
+  
+  /* User will use his/her email as username */
+  { 
+    usernameField: "email"
+  },
+
+  function(email, password, done) {
+      /* When user signs in */
+      db.User.findOne({
+          where: {
+            email: email
+          }
+      }).then(function(dbUser) {
+            /* If there is no user with the email provided */
+            if(!dbUser) {
+                return done(null, false, {
+                    message: "Incorrect email!"
+                });
+
+            /* If the user types in the wrong password, but email provided is valid */
+            } else if(!dbUser.validPassword(password)) {
+                return done(null, false, {
+                    message: "Incorrect password!"
+                });
+            }
+
+            return done(null, dbUser);
+
+        });
+    }
+
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/* Export configured passport model */
+module.exports = passport;
+
 
 /* Create MongoDB connection */
---
+++ server/models/Game.js
@@ -0,0 +1,29 @@
+/*
+*
+*
+* Mongoose Middleware File/Set Up Schema 
+*
+*
+*/
+// Require mongoose
+var mongoose = require("mongoose");
+/* Import dependencies, require mongoose */
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  // name: a unique String
+  username: {
+    type: String,
+    unique: true
+  },
+  password: [{
+    type: String,
+    unique: true
+  }]
+});
+
+/* Set up database schema */
+var Game = mongoose.model("Game", UserSchema);
+
+// Export the user model
+module.exports = Game;
