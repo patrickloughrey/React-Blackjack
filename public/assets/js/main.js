6dcc4490f907eced2d9cb4b6017290dd41398dfb
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2017-05-05 19:47:09 -0400

    Styled the modal form

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -31,3 +31,21 @@ $(document).ready( () => {
 	});
 
 });
+
+/* Switching between login and sign up tabs */
+$(document).ready(function(){
+	$(document).on('click','.signup-tab',function(e){
+		e.preventDefault();
+	    $('#signup-taba').tab('show');
+	});	
+	
+	$(document).on('click','.signin-tab',function(e){
+	   	e.preventDefault();
+	    $('#signin-taba').tab('show');
+	});
+	    	
+	$(document).on('click','.forgetpass-tab',function(e){
+	 	e.preventDefault();
+	   	$('#forgetpass-taba').tab('show');
+	});
+});	
